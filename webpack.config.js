b570fdaddda41b6652aad00c6ac8ef7fa68f22ea
Anton Bachurin <ant.bachurin@gmail.com>
2024-04-04 18:00:47 +0400

    не показывает содержимое html страницы

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, './src/index.js'),
+    mode: 'development',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 9000,
     },
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
     output: {
         path: path.resolve(__dirname, './dist'),
         filename: 'index_bundle.js',
-    }
-}
+    },
+};
